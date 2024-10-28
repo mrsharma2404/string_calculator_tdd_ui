b6346039a071d60100e3785a213c78cf27466b13
mrsharma2404 <mrsharma2404@egmail.com>
2024-10-28 11:22:50 +0530

    added second test case

--- src/app/tests/StringCalculator.test.ts
+++ src/app/tests/StringCalculator.test.ts
@@ -3,3 +3,7 @@ import StringCalculator from "../helpers/StringCalculator";
 test("should calculate the sum of positive numbers", () => {
   expect(StringCalculator("1,2,3")).toBe(6);
 });
+
+test("should return 0 if empty string", () => {
+  expect(StringCalculator("")).toBe(0);
+});
