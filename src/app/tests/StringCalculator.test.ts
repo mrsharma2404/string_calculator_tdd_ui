62f4ffe2df9deb678a35a7af35f1fb5cc2af45c1
mrsharma2404 <mrsharma2404@egmail.com>
2024-10-28 11:25:39 +0530

    added test cases for only blankspace and comma with blankspace is present

--- src/app/tests/StringCalculator.test.ts
+++ src/app/tests/StringCalculator.test.ts
@@ -7,3 +7,10 @@ test("should calculate the sum of positive numbers", () => {
 test("should return 0 if empty string", () => {
   expect(StringCalculator("")).toBe(0);
 });
+
+test("should return 0 if only blankspace is present", () => {
+  expect(StringCalculator("  ")).toBe(0);
+});
+test("should return 0 if comma with blankspace is present", () => {
+  expect(StringCalculator("  ,  ")).toBe(0);
+});
