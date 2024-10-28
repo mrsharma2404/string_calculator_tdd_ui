0ca9d8f03470c963fbba815cc5e41c4b39487b4e
mrsharma2404 <mrsharma2404@egmail.com>
2024-10-28 11:27:38 +0530

    added test case for negative numbers and wrote code accordingly

--- src/app/helpers/StringCalculator.ts
+++ src/app/helpers/StringCalculator.ts
@@ -1,9 +1,19 @@
 function StringCalculator(input: string): number {
   let sum: number = 0;
+  const negatives: number[] = [];
+
   input.split(/[,|\n]/).forEach((num) => {
     const number = Number(num);
-    sum += number;
+    if (number < 0) {
+      negatives.push(number);
+    } else {
+      sum += number;
+    }
   });
+
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed: ${negatives.join(", ")}`);
+  }
   return sum;
 }
 export default StringCalculator;
--- src/app/tests/StringCalculator.test.ts
+++ src/app/tests/StringCalculator.test.ts
@@ -18,3 +18,9 @@ test("should return 0 if comma with blankspace is present", () => {
 test("should handle newline characters", () => {
   expect(StringCalculator("1\n2,3")).toBe(6);
 });
+
+test("should throw an error for negative numbers", () => {
+  expect(() => StringCalculator("1,-2,-3,4")).toThrow(
+    "negative numbers not allowed: -2, -3"
+  );
+});
